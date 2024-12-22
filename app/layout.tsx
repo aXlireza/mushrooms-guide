8f50649f2f7d4c7ba4e2a8ae86b8b50699baf281
aXlireza <alireza.ataee01@gmail.com>
2024-12-22 19:50:37 +0330

    Cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
-import { AppSidebar } from '@/components/app-sidebar'
 import './globals.css'
 import Header from '@/components/header'
 import { ThemeProvider } from "@/components/theme-provider"
-import { SidebarProvider } from '@/components/ui/sidebar'
 
 interface RootLayoutProps {
   children: React.ReactNode
@@ -14,15 +12,12 @@ export default function RootLayout({
   return (
     <html lang="fa" dir="rtl" suppressHydrationWarning>
       <body className="min-h-screen">
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            {/* <SidebarProvider>
-              <AppSidebar /> */}
-              <main>
-                <Header />
-                {children}
-              </main>
-            {/* </SidebarProvider> */}
-          </ThemeProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <main>
+            <Header />
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   )
